5495c275f7182bc0e49a0823b623f986d2beee42
Noel-H <noel.huberto@gmail.com>
2022-09-12 14:53:11 +0200

    Initial commit

---
+++ src/main/java/com/noelh/mediscreendiabetes/MediscreenDiabetesApplication.java
@@ -0,0 +1,13 @@
+package com.noelh.mediscreendiabetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenDiabetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediscreenDiabetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noelh/mediscreendiabetes/MediscreenDiabetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelh.mediscreendiabetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenDiabetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
